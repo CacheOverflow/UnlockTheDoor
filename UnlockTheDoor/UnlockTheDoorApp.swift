fa58d882e30b8ec6fa4c68b2b061b674c0cea175
Daniel Tirzuman <github@tirzuman.com>
2025-09-07 20:22:06 +0300

    Initial Commit

---
+++ UnlockTheDoor/UnlockTheDoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnlockTheDoorApp.swift
+//  UnlockTheDoor
+//
+//  Created by TirzumanDaniel on 07.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct UnlockTheDoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnlockTheDoorTests/UnlockTheDoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnlockTheDoorTests.swift
+//  UnlockTheDoorTests
+//
+//  Created by TirzumanDaniel on 07.09.2025.
+//
+
+import Testing
+@testable import UnlockTheDoor
+
+struct UnlockTheDoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
